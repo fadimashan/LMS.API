dd66444c64c2ad1225d9d5d657dbfa0c124ad9ba
Fadi Mashan <fadi.mashan@gmail.com>
2021-04-14 11:44:04 +0200

    Add project files.

---
+++ LMS.Data/Data/LMSAPIContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LMS.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Data.Data
+{
+    public class LMSAPIContext : DbContext
+    {
+
+        public LMSAPIContext (DbContextOptions<LMSAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Course> Course { get; set; }
+
+        public DbSet<Module> Module { get; set; }
+    }
+}
